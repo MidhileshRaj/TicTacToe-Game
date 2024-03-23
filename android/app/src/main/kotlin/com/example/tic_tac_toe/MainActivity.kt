0179f77cc1f1bef8eb15b1d5a67c0b72462fa405
MidhileshRaj <midhileshraj01@gmail.com>
2024-03-23 10:00:44 +0530

    project started

---
+++ android/app/src/main/kotlin/com/example/tic_tac_toe/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tic_tac_toe
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.amberAccent,
+        primarySwatch: Colors.amber,
+
+      ),
+    );
+  }
+}
